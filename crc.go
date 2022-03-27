1df49814903210db4da40443dad74fbd94385c70
Tamal Saha <tamal@appscode.com>
2022-03-26 22:52:13 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- crc.go
+++ crc.go
@@ -78,6 +78,7 @@ var crc16tab = [256]uint16{
 	0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
 }
 
+// ref: https://redis.io/docs/reference/cluster-spec/#appendix
 func crc16(buf []byte, len int) uint16 {
 	var upper byte
 	var crc uint16 = 0
